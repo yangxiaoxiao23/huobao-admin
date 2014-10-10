34f7024b98d7993dc4f4099ade533cf3b0f84e67
yangxiaoxiao23 <yangxiaoxiao23@gmail.com>
2014-10-10 16:16:16 +0800

    v1.0

--- js/largedata/user-manager.js
+++ js/largedata/user-manager.js
@@ -3,33 +3,48 @@
  */
 
 $(function(){
-    /*业主导入*/
-    $('#J-owner-import').click(function(){
 
+    // 业主导入
+    $('#J-owner-import').uploadify({
+        'swf': '../../libs/uploadify/uploadify.swf',
+        'uploader': 'uploadify.php',
+        buttonText: '<div>导入</div>'
     });
 
-    /*业主导出*/
-    $('#J-owner-export').click(function(){
-
+    // 业主导出
+    $('#J-owner-export').uploadify({
+        'swf': '../../libs/uploadify/uploadify.swf',
+        'uploader': 'uploadify.php',
+        buttonText: '<div>导出</div>'
     });
 
-    /*家属导入*/
-    $('#J-family-import').click(function(){
 
+    // 家属导入
+    $('#J-family-import').uploadify({
+        'swf': '../../libs/uploadify/uploadify.swf',
+        'uploader': 'uploadify.php',
+        buttonText: '<div>导入</div>'
     });
 
-    /*家属导出*/
-    $('#J-family-export').click(function(){
-
+    // 家属导出
+    $('#J-family-export').uploadify({
+        'swf': '../../libs/uploadify/uploadify.swf',
+        'uploader': 'uploadify.php',
+        buttonText: '<div>导出</div>'
     });
 
-    /*租客导入*/
-    $('#J-renter-import').click(function(){
 
+    // 租客导入
+    $('#J-renter-import').uploadify({
+        'swf': '../../libs/uploadify/uploadify.swf',
+        'uploader': 'uploadify.php',
+        buttonText: '<div>导入</div>'
     });
 
-    /*租客导出*/
-    $('#J-renter-export').click(function(){
-
+    // 租客导出
+    $('#J-renter-export').uploadify({
+        'swf': '../../libs/uploadify/uploadify.swf',
+        'uploader': 'uploadify.php',
+        buttonText: '<div>导出</div>'
     });
 });
