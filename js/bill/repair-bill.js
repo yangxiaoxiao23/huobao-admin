3c07382fa48566b175b203b23d348f43ff884613
yangxiaoxiao23 <yangxiaoxiao23@gmail.com>
2014-10-10 14:17:36 +0800

    v1.0

--- js/bill/repair-bill.js
+++ js/bill/repair-bill.js
@@ -1,3 +1,40 @@
 /**
  * Created by ASRock on 2014/10/10.
  */
+
+
+$(function(){
+    /* 新增 滑动侧边栏*/
+    /*$('.sidebar').first().sidebar('attach events', '.reply-btn').sidebar('setting', {
+        onShow: function(){
+            return;
+        },
+        onHide: function () {
+        },
+        duration: 0
+    });*/
+    
+    $('.reply-btn').click(function () {
+
+        alert('//TODO 改变编号');
+
+        $('.sidebar').sidebar('show');
+    });
+
+    /* 重置  */
+    $('#J-reset-form').click(function () {
+        $('#content').val('');
+    });
+
+    var url ;
+
+    /* 提交  */
+    $('#J-submit-form').click(function () {
+        $.post(url, {
+            action: 'addBillReply',
+            content: $('#content').val()
+        }, function () {
+
+        });
+    });
+});
